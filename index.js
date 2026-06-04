d36ffed442a943379dfb05d059819972869649c9
sebastian123k <159938783+sebastian123k@users.noreply.github.com>
2026-06-03 20:36:14 -0600

    push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const JWT_SECRET = process.env.JWT_SECRET || 'bcs_turismo_secret_2026';
-const NODO_NOMBRE = 'Guadalupe';
+const NODO_NOMBRE = 'sebas';
 const NODO_URL = 'https://bcs-backend-guadalupe-production-3f83.up.railway.app';
 
 const app = express();
@@ -22,7 +22,10 @@ const pool = new Pool({
 });
 
 const NODOS_REPLICAS = [
+  'https://bcs-backend-guadalupe-production.up.railway.app',
   'https://bcs-backend-juan-production.up.railway.app',
+  'https://bcs-backend-gama-production.up.railway.app',
+  'https://bcs-backend-adan-production.up.railway.app',
 ];
 
 // Guardar en log de replicación
